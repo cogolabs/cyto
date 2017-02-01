297603d7be03b357c775729f7fa1603fad53b29e
Connor Taylor <connor@cogolabs.com>
2017-02-01 12:43:26 -0500

    removing logging from renderDependency

--- src/dependencies/renderDependency/renderDependency.js
+++ src/dependencies/renderDependency/renderDependency.js
@@ -5,12 +5,10 @@
  */
 import fs from 'fs';
 import path from 'path';
-import chalk from 'chalk';
 
 import loadTemplate from '../../template/loadTemplate';
 
 import renderString from '../../utils/render/renderString';
-import log from '../../utils/log';
 
 /**
  * Renders the file that a dependency refers to and write it to the desired
@@ -20,15 +18,12 @@ import log from '../../utils/log';
  * @param {string} outputRoot - Where to write the rendered content
  * @param {Object} args - Args for the renderer to use
  */
-export default function renderDependency(dep, outputRoot, args, initialRoot) {
+export default function renderDependency(dep, outputRoot, args) {
   const [name, templateId] = dep;
   const outputPath = renderString(
     path.join(outputRoot, name),
     args,
   );
-
-  const relativePath = outputPath.replace(`${initialRoot}/`, '');
-  log.info(`Writing ${chalk.green(relativePath)}`);
 
   const template = loadTemplate(templateId);
 
