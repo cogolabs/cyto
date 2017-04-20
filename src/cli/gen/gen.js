a98859d00c5697e96f3acb656bc206ae7b71e0b2
connor <cmtaylor1123@gmail.com>
2017-04-19 22:53:03 -0400

    fixed bug with gen

--- src/cli/gen/gen.js
+++ src/cli/gen/gen.js
@@ -21,7 +21,7 @@ export default function gen(program: Object) {
     .alias('generate')
     .description('Generate a cyto template')
     .option('-o, --output [val]', 'Where to output the template')
-    .action(async (templateString, id, args, options) => {
+    .action(async (templateString, id, options) => {
       // Returns an object where the keys are filepaths and the values are
       // the rendered dependencies that should be written to those filepaths
       const generatedTemplate = await generateTemplate({
--- src/template/generateTemplate/generateTemplate.js
+++ src/template/generateTemplate/generateTemplate.js
@@ -69,6 +69,7 @@ export default async function generateTemplate(options: GenerateOptions) {
 
       return { ...accum, ...generatedTemplate };
     }
+
     const renderedDep = await renderDependency( // 5b
       dep,
       outputRoot,
