f9dab77c23c189227b8d89c8889cee6bcc9d93cc
Connor Taylor <connor@cogolabs.com>
2017-05-03 16:09:48 -0400

    wrote more unit tests

--- src/configs/mergeCytoConfigs/mergeCytoConfigs.test.js
+++ src/configs/mergeCytoConfigs/mergeCytoConfigs.test.js
@@ -3,6 +3,26 @@
  * mergeCytoConfigs.test.js
  * Written by: Connor Taylor
  */
+import mocks from './mocks';
+import mergeCytoConfigs from './mergeCytoConfigs';
+
+import mergeArgs from '../../args/mergeArgs';
+import mergeDependencies from '../../dependencies/mergeDependencies';
+
+jest.mock('../../args/mergeArgs');
+jest.mock('../../dependencies/mergeDependencies');
 
 describe('mergeCytoConfigs', () => {
+  it('calls mergeArgs and mergeDependencies', () => {
+    mergeCytoConfigs(mocks.CONFIG, mocks.BASE_CONFIG);
+
+    expect(mergeArgs).toBeCalled();
+    expect(mergeDependencies).toBeCalled();
+  });
+
+  it('does not copy the base template options', () => {
+    const newConfig = mergeCytoConfigs(mocks.CONFIG, mocks.BASE_CONFIG);
+
+    expect(newConfig.options).toEqual({ foo: 'bar' });
+  });
 });
