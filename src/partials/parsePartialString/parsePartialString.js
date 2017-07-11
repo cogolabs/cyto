d3b1103e071d0f82520d81b201da467969e10472
Connor Taylor <connor@cogolabs.com>
2017-07-11 12:16:05 -0400

    revert changes to parsePartialString

--- src/partials/parsePartialString/parsePartialString.js
+++ src/partials/parsePartialString/parsePartialString.js
@@ -10,27 +10,23 @@ import fumanchu from 'fu-manchu';
  * to allow for dynamic setting of the template or the template id.
  */
 const parsePartialString = async (partialString, context) => {
-  try {
-    const renderedString = await fumanchu.render(partialString, context);
-    const tokens = renderedString.split(' ').filter((s) => s.trim());
-    if (!tokens.length || tokens.length > 2) {
-      throw new Error(`Invalid partial string ${partialString}`);
-    } else if (tokens.length === 1) {
-      if (context.id) {
-        tokens.push(context.id);
-      } else {
-        throw new Error(`${partialString} has no id.
-  Either set one explicity or make sure that the partial's rendering context has an id`);
-      }
+  const renderedString = await fumanchu.render(partialString, context);
+  const tokens = renderedString.split(' ').filter((s) => s.trim());
+  if (!tokens.length || tokens.length > 2) {
+    throw new Error(`Invalid partial string ${partialString}`);
+  } else if (tokens.length === 1) {
+    if (context.id) {
+      tokens.push(context.id);
+    } else {
+      throw new Error(`${partialString} has no id.
+Either set one explicity or make sure that the partial's rendering context has an id`);
     }
-
-    const [rawId, id] = tokens;
-    const templateId = await fumanchu.render(rawId, context);
-
-    return [templateId, id];
-  } catch (e) {
-    throw new Error(e);
   }
+
+  const [rawId, id] = tokens;
+  const templateId = await fumanchu.render(rawId, context);
+
+  return [templateId, id];
 };
 
 export default parsePartialString;
